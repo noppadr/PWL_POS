600dd885681245fd295f45d16e91059ecb793a0b
Nova Diana <nopadr12@gmail.com>
2025-03-14 05:10:38 +0700

    prak 6 - Implementasi Eloquent ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function profile($id, $name)
     {
-        return view('user.profile', [
-            'id' => $id,
-            'name' => $name
-        ]);
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
