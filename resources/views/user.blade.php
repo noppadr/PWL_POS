d5db3b93212b4cef772843b8844be7f3efd04faa
Nova Diana <nopadr12@gmail.com>
2025-03-14 12:22:51 +0700

    JS 4 - Prak 2.3 (Retreiving Aggregrates)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,24 +1,18 @@
 <!DOCTYPE html>
-<html lang>
+<html>
 <head>
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    <h2>Data User</h2>
+    <table border="1">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>Jumlah Pengguna</th>
         </tr>
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-    </table> 
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
+    </table>
 </body>
 </html>
 
