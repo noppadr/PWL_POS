98a337326ff557db8c0dc981c296037e31a074d2
Nova Diana <nopadr12@gmail.com>
2025-03-15 22:40:38 +0700

    JS 4 - Prak 2.5 - Langkah 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,31 +10,24 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate(
+        $user = UserModel::create(
             [
                 'username' => 'manager11',
-                'nama' => 'Manager55',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2,
             ]);
 
-            $user->username = 'manager56';
+            $user->username = 'manager12';
 
-            $user->isDirty(); //true
-            $user->isDirty('username'); //true
-            $user->isDirty('nama'); //false
-            $user->isDirty('nama', 'username'); //true
-            
-            $user->isClean(); //false
-            $user->isClean('username'); //false
-            $user->isClean('nama'); //false
-            $user->isClean(['nama', 'username']); //false          
-            
             $user->save();
 
-            $user->isDirty(); //false
-            $user->isClean(); //true
-            dd($user->isDirty());
+            $user->wasChanged(); //true
+            $user->wasChanged('username'); //true
+            $user->wasChanged('username', 'level_id'); //true
+            $user->wasChanged('nama'); //false
+            dd($user->wasChanged(['nama', 'username'])); //true   
+
     }
 
 }
