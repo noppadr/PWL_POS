cb8db821fc0931e27ed8f5b57bcbe23c3e8236c5
Nova Diana <nopadr12@gmail.com>
2025-03-16 21:04:49 +0700

    JS 4 - Prak 2.6 - tambah

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,26 +10,25 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ]);
-
-            $user->username = 'manager12';
-
-            $user->save();
-
-            $user->wasChanged(); //true
-            $user->wasChanged('username'); //true
-            $user->wasChanged('username', 'level_id'); //true
-            $user->wasChanged('nama'); //false
-            dd($user->wasChanged(['nama', 'username'])); //true   
-
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) 
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
 }
     
 
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 
 
