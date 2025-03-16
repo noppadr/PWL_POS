e07828ce47999ff83a3fbdce49ed7996f115c5b3
Nova Diana <nopadr12@gmail.com>
2025-03-16 21:42:20 +0700

    JS 4 - Prak 2.6 - hapus user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -29,7 +30,36 @@ class UserController extends Controller
         ]);
         return redirect('/user');
     }
+
+    public function ubah($id) 
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        
+        return redirect('/user');
+    }
 }
+
     
 
 
