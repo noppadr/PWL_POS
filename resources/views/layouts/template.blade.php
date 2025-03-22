a571d0850ea3b949b3582ccbf4e2788ab77a8821
Nova Diana <nopadr12@gmail.com>
2025-03-22 13:15:43 +0700

    JS 5 - Prak 2 Penerapan Layouting

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -15,15 +15,15 @@
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
-    <!-- Navbar -->
-    @include('layouts.header')
-    <!-- /.navbar -->
+  <!-- Navbar -->
+  @include('layouts.header')
+  <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ assets('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
@@ -39,7 +39,7 @@
 
     <!-- Main content -->
     <section class="content">
-        @yield('content')
+      @yield('content')
     </section>
     <!-- /.content -->
   </div>
