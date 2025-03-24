41ca6a89cd50a15eaebb709b9776b520e3bd6283
Nova Diana <nopadr12@gmail.com>
2025-03-25 03:20:09 +0700

    JS 5 - Tugas Tabel m_supplier

---
+++ resources/views/supplier/index.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3>{{$page -> title}}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{url('supplier/create')}}">Tambah</a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{session('success')}}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{session('error')}}</div>
+            @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select class="form-control" id="supplier_id" name="supplier_id" required>
+                                <option value="">- Semua -</option>
+                                @foreach ($supplier as $item)
+                                    <option value="{{ $item->supplier_id }}">{{ $item->supplier_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Supplier Kode</th>
+                        <th>Supplier Nama</th>
+                        <th>Supplier Alamat</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+@endsection
+
+@push('css')
+@endpush
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            var dataSupplier = $('#table_level').DataTable({
+                serverSide: true,
+                ajax: {
+                    "url": "{{url('supplier/list')}}",
+                    "dataType": "json",
+                    "type": "POST",
+                    "data": function(data) {
+                        data.supplier_id = $('#supplier_id').val();
+                    }
+                },
+                columns: [
+                    {
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "supplier_kode",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "supplier_nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "supplier_alamat",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+
+            $('#supplier_id').on('change', function() {
+                dataSupplier.ajax.reload();
+            });
+        });
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,14 @@ Route::prefix('kategori')->group(function () {
     Route::put('/{id}', [KategoriController::class, 'update']);
     Route::delete('/{id}', [KategoriController::class, 'destroy']);
 });
+
+Route::prefix('supplier')->group(function () {
+    Route::get('/', [SupplierController::class, 'index']);
+    Route::post('/list', [SupplierController::class, 'list']);
+    Route::get('/create', [SupplierController::class, 'create']);
+    Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/{id}', [SupplierController::class, 'show']);
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);
+    Route::put('/{id}', [SupplierController::class, 'update']);
+    Route::delete('/{id}', [SupplierController::class, 'destroy']);
+});
